0b16c38432f35cb0522ed82c9f2f431496870899
abdullaO7 <abdallahalhasan09@gmail.com>
2025-01-25 21:12:07 +0300

    updated the models

--- models/BlogsCategories.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const BlogsCategoriesSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-}, {timestamps : true});
-
-module.exports = mongoose.model('BlogsCategories', BlogsCategoriesSchema);
